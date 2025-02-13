69a3b7fb139eb194f65b94266951be8232b48674
Aditya Kumar <adityaicd2016@gmail.com>
2025-02-13 10:31:19 +0530

    basic setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+dotenv.config();
+connectDB(); // db connection
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
